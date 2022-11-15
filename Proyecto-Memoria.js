8187c5e133effbfed781627b0269e42cdb05931b
Carlos Morales <carlosmora230190@gmail.com>
2022-11-14 23:32:28 -0300

    Se agrego funcion voltear cartas diferentes

--- Proyecto-Memoria.js
+++ Proyecto-Memoria.js
@@ -19,8 +19,6 @@ let numeros = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
 
 //uso la propiedad Sort para crear un arreglo ordenado aleatoriamente del arreglo anterior
 numeros = numeros.sort(()=> {return Math.random()-0.5});
-
-
 console.log(numeros)
 
 //Funcion Principal
@@ -60,11 +58,14 @@ function destapar(id){
             mostrarAciertos.innerHTML = `Aciertos: ${aciertos}`;
         }
         else{
-            //Mostrar momentaneamente valores y volver a tapar 
+            //Mostrar momentaneamente valores y volver a tapar
+            setTimeout(() =>{
+                tarjeta1.innerHTML = ' ';
+                tarjeta2.innerHTML = ' ';
+                tarjeta1.disabled = false;
+                tarjeta2.disabled = false;
+                tarjetasDestapadas = 0;
+            },750);
         }
-
-
     }
-
-
 }
